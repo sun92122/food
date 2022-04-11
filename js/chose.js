3c0679268d3c55c82c6b93c4380afc7390aec1ec
magical <sun920122@gmail.com>
2022-04-11 20:44:14 +0800

    fixed checkbox bug

--- js/chose.js
+++ js/chose.js
@@ -4,9 +4,9 @@ import './jquery/jquery-2.2.4.js';
 for (var i = 0; i < key.length; i++) {
     $('.checkbox-group').append(
     `<input id="${key[i]}" type="checkbox" class="jq-checked">`+
-    `<label for="${key[i]}" class="boxtext">`+
+    `<label for="${key[i]}">`+
     `<span class="icon-checkbox"></span><span>${key[i]}: ${dict[key[i]]}</span>`+
     '</label>'
     )
 };
-$(".jq-checked").prop("checked", true);
+//$(".jq-checked").prop("checked", true);
